c440ed1d47e1e2cae745aa1504eafade41e62421
ben-m-m <mainaben23@gmail.com>
2026-09-12 02:12:49 +0200

    Refactor HBNBCommand class: clean up whitespace and enhance docstring for clarity

--- console.py
+++ console.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """Command Line Interpreter for the AirBNB clone"""
     prompt = "(hbnb)"
 
     def do_help(self, arg):
         """prints helpful info on usable commands"""
-        
         if arg == "quit":
             print("Quit command to exit the program\n")
         elif arg == "EOF":
@@ -17,13 +18,11 @@ class HBNBCommand(cmd.Cmd):
             print("Documented commands (type help <topic>):")
             print("=======================================")
             print("EOF help quit\n")
-    
-
 
     def do_quit(self, arg):
         """exits the program"""
-        
         return True
+
     def do_EOF(self, arg):
         """same as quit: exits the program"""
         return True
@@ -31,6 +30,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """prevents the execution of the last command"""
         return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
